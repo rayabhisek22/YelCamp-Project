b2f7cc55c891b3c0987462a8825c8e343bc1a319
Abhisek Ray <rayabhisek22@gmail.com>
2019-06-10 16:11:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,97 @@
-var express = require('express');
-var app = express();
-var request = require('request');
-var bodyParser = require('body-parser');
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
-var campground = [
-  {name:"Amarnath" ,img:"http://images.jagran.com/amarnath-baltal-b.jpg" },
-  {name:"Kedarnath" ,img:"https://www.sacredyatra.com/wp-content/uploads/2015/04/Closer-View-of-Tents-in-Kedarnath.jpg" },
-  {name:"Shillong" ,img:"https://www.mountainmarttreks.com/pagegallery/10-days-everest-base-camp-trek85.jpg" }
-];
+var express    = require('express'),
+    app        = express(),
+    request    = require('request'),
+    bodyParser = require('body-parser'),
+    mongoose   = require('mongoose');
 
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Mongo DB setup===================================================================================================
+mongoose.connect("mongodb://localhost:27017/yel_camp",{useNewUrlParser:true},function(err){
+  if(err) console.log("error");
+  else {
+    console.log("connected!");
+  }
+});
+
+var schema = mongoose.Schema;
+var campgroundSchema = new schema({
+  name : String,
+  img : String,
+  description : String
+});
+
+var campground = mongoose.model("campground",campgroundSchema);
+
+//RESTFUL ROUTES=====================================================================================================
+
+//HOME============================================
 app.get("/",function(req,res){
   res.render("home");
   console.log("Requested the home page");
 });
 
+//ABOUT============================================
+app.get("/about",function(req,res){
+  res.render("about");
+  console.log("Requested the home page");
+});
+
+//INDEX============================================
 app.get("/campground",function(req,res){
-  res.render("campground",{campground:campground});
+
+  campground.find({},function(err,allCampground){
+    if(err){
+      console.log("Couldn't find!!");
+      console.log(err);
+    }else{
+      res.render("index",{campground:allCampground});
+    }
+  });
+
   console.log("Requested the campground page");
 });
 
+//NEW============================================
 app.get("/campground/new",function(req,res){
   res.render("new");
   console.log("Requested the add new campground page");
 });
 
+//CREATE=========================================
 app.post("/campground",function(req,res){
     var newname = req.body.name;
     var newimg = req.body.img;
+    var newdescription = req.body.description;
 
-    var newCampground = {name: newname, img: newimg};
-    campground.push(newCampground);
-    res.redirect("/campground");
+    var newCamp = {name:newname,img:newimg,description:newdescription};
+
+    campground.create(newCamp,function(err,camp){
+      if(err){
+        console.log("Error!");
+      }else{
+        console.log("Created!");
+        res.redirect("campground");
+      }
+    });
+
 });
 
+//SHOW======================================
+app.get("/campground/:id",function(req,res){
+  campground.findById(req.params.id,function(err,foundCampground){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("show",{campground:foundCampground});
+    }
+  });
+
+});
+
+//LISTEN=================================================================================================================
 app.listen(8080,function(){
   console.log("Yelcamp has started");
 });
